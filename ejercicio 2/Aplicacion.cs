a673b55a5ef55f5f9e1c13894d0325a9461af4a0
rafasuman <rleonardomartinez@unicesar.edu.co>
2020-09-11 16:07:44 -0500

    first commit

---
+++ ejercicio 2/Aplicacion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicio_2
+{
+
+    class Aplicacion
+    {
+        public Aplicacion()
+        {
+            RecibirMedidas();
+        }
+        public Medidas  ObtenerMedida()
+        {
+            Console.WriteLine("Digite su edad");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite su genero");
+            string genero = Console.ReadLine();
+            Medidas medidas = new Medidas(edad,genero);
+            medidas.ObtenerPulsaciones();
+            return medidas;
+        }
+
+        public void RecibirMedidas()
+        {
+            Medidas medidas = ObtenerMedida();
+            int pulsaciones = medidas.Pulsaciones;
+            Console.WriteLine(pulsaciones);
+        }
+    }
+}
---
+++ ejercicio 2/Medidas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicio_2
+{
+    class Medidas
+    {
+        public int Edad { get; set; }
+        public string Genero { get; set; }
+        public int Pulsaciones { get; set; }
+
+        public Medidas(int edad,string genero)
+        {
+            Edad = edad;
+            Genero = genero;
+        }
+
+        public int ObtenerPulsaciones()
+        {
+            if (Genero.Equals("F"))
+                Pulsaciones= (220 - Edad) / 10;
+            else
+                Pulsaciones = (210 - Edad) / 10;
+            return Pulsaciones;
+        }
+    }
+}
